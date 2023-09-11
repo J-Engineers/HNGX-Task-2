9f3b2ad3e78efc31b4848094771ccbe2f898723c
J-Engineers <ugboguj@yahoo.com>
2023-09-11 06:48:52 +0100

    Making a get endpoint to fetch all records from persons database table

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Person;
 
 class PersonController extends Controller
 {
     //
+
+    public function index(){
+        $person = Person::all();
+
+        $personData = [];
+        $statusData = 404;
+        $message = "No Records Found";
+
+        if($person->count() > 0){
+            $personData = $person;
+            $statusData = 200;
+            $message = $person->count()." Records Found";
+        }
+
+        $data = [
+            'status' => $statusData,
+            'message' => $message,
+            'person' => $personData
+        ];
+        
+        return response()->json($data, 200);
+    }
+
 }
